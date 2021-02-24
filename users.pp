d7448dbca74870c963cd9f0fdb46b958c72caf57
root <root@master.openstacklocal>
2021-02-24 19:02:41 +0000

    updated the user puppet file

--- users.pp
+++ users.pp
@@ -1,16 +1,26 @@
+group { 'webadmins':
+    ensure => present,
+}
+
 user { 'tom':
     ensure => present,
-    groups => ['sudo'],
+    groups => ['sudo', 'webadmins'],
+    home => '/home/tom',
+    managehome => true,
     password => '$1$iSvgT7vh$SXk7J3c54RxOG6A13KqhV/',
 }
 
 user { 'brady':
     ensure => present,
-    groups => ['sudo', 'admin'],
-    password => Sensitive("bradypw"),
+    groups => ['sudo', 'webadmins'],
+    managehome => true,
+    home => '/home/brady',
+    password => '$1$8ZWY1RD1$xWUayN/Ce.VodCQR8yVpH/',
 }
 
 user { 'janet':
     ensure => present,
-    password => Sensitive("janetpw"),
+    groups => ['sudo', 'webadmins'],
+    managehome => true,
+    password => '$1$RGQZA46h$IFLcbEC3K6dcXjBmG68Ug/',
 }
